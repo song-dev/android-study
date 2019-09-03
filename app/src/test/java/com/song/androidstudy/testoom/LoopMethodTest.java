a93f090c5377937b8cc7e388a9aa6236ed7f3f8c
chensongsong <chensongsong@geetest.com>
2019-09-03 17:41:36 +0800

    oom 测试

---
+++ app/src/test/java/com/song/androidstudy/testoom/LoopMethodTest.java
@@ -0,0 +1,81 @@
+package com.song.androidstudy.testoom;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+/**
+ * Created by chensongsong on 2019/9/2.
+ */
+public class LoopMethodTest {
+
+    private int count = 0;
+
+    private void baseMethod() {
+        count++;
+        baseMethod();
+    }
+
+    @Test
+    public void test_loop() {
+
+        try {
+            baseMethod();
+        } catch (Throwable throwable) {
+            System.out.println("count = " + count);
+            throwable.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void test_heap_oom() {
+
+        try {
+            ArrayList<Object> list = new ArrayList<>();
+            while (true) {
+                list.add(new Object());
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+    }
+
+    static class OOMObject {
+    }
+
+    @Test
+    public void test_method_area() {
+
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invoke(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+
+    }
+
+    @Test
+    public void test_intern() {
+
+        String s = new StringBuffer().append("haha").append("哈哈").toString();
+        System.out.println(s == s.intern());
+
+        String s2 = new StringBuffer().append("Ja").append("va").toString();
+        System.out.println(s2 == s2.intern());
+
+    }
+}
